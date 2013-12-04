b53badb1ad7f28c9a91103a51118b4ca90d47647
Thiago Marcos P. Santos <thiago.santos@intel.com>
2013-12-04 09:51:08 +0200

    [SysApps] Watch for displays connections and disconnections
    Using the Chromium's gfx:: abstractions on top of Screen and Display,
    we can watch for new connections and disconnections in a portable way.
    
    This patch also adds an observer interface to DisplayInfoProvider so
    the DeviceCapabiltiesObject can subscribe to this events when necessary.

---
+++ sysapps/device_capabilities_new/display_info_provider_android.cc
@@ -0,0 +1,11 @@
+// Copyright (c) 2013 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+namespace gfx {
+
+// FIXME(tmpsantos): We can probably remove this hack after fixing
+// https://code.google.com/p/chromium/issues/detail?id=326140
+DisplayObserver::~DisplayObserver() {}
+
+}  // namespace gfx
