81ba266e368754dc41bd36f1e1fa1481ec11abb7
Sudarsana Nagineni <sudarsana.nagineni@intel.com>
2013-12-13 14:02:01 +0200

    [SysApps] Fix a build error on Android.
    Include ui/gfx/display_observer.h to avoid a build error on Android.

--- sysapps/device_capabilities_new/display_info_provider_android.cc
+++ sysapps/device_capabilities_new/display_info_provider_android.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ui/gfx/display_observer.h"
+
 namespace gfx {
 
 // FIXME(tmpsantos): We can probably remove this hack after fixing
